360013552f66577b7ac0661eb7a8c883ad2854be
Okuribi <47673662+Okuribi@users.noreply.github.com>
2020-06-02 13:39:13 -0700

    Delete random_agent.py
    This thing is useless.

--- random_agent.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from agent import Agent
-
-class RandomAgent(Agent):
-    def step(self, obs):
-        super(RandomAgent, self).step(obs)
-        action = random.choice(self.my_actions)
-        return getattr(self, action)(obs)
